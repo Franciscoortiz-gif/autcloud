844dc519e7c54b49e3fe3434779d1d1dc82a88fc
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-26 02:00:00 -0600

    Update login.js

--- js/login.js
+++ js/login.js
@@ -11,4 +11,27 @@ document.getElementById("loginform").addEventListener("submit", (event) => {
   
 });
 
+document.getElementById("recober").addEventListener("click", (e)=>{
+  e.preventDefault();
+  const email = document.getElementById("floatingInput1").value;
+  const respass = new ManageAccount();
+  respass.resetpass(email);
+});
+
+const pass = document.getElementById("floatingPassword"),
+      icon = document.getElementById("iconeye1");
+
+icon.addEventListener('click', (e) =>{
+  if(pass.type === "password"){
+    pass.type = "text";
+    icon.classList.remove("bx-show-alt");
+    icon.classList.add("bx-hide");
+  }else{
+    pass.type = "password";
+    icon.classList.remove("bx-hide");
+    icon.classList.add("bx-show-alt");
+    
+  }
+})
+
 console.log('Formulario de Inicio de Sesión');
