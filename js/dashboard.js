bf606a8f5e25a496361aea7e544c51b74b06e1b8
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-27 14:52:45 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,3 +3,15 @@ export const nameuser = document.getElementById("username");
 export const emailuser = document.getElementById("useremal");
 export const imageuser = document.getElementById("profid");
 
+export const open = document.getElementById("add");
+export const page = document.getElementById("addnew");
+export const close = document.getElementById("save");
+
+open.addEventListener("click", () =>{
+    page.classList.add('show');
+    alert("prueba");
+})
+
+close.addEventListener("click", () =>{
+    page.classList.remove('show');
+})
