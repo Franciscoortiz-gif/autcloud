7727e4e2374b6789c8aca7ff75b922870dd1d0c1
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-27 18:55:57 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -6,13 +6,8 @@ export const imageuser = document.getElementById("profid");
 const open = document.getElementById("add");
 const page = document.getElementById("addnew");
 const close = document.getElementById("save");
-const nombredev = document.getElementById("nombre").value;
-let activo = document.getElementById("activo").value;
-let ip = document.getElementById("Ip").value;
-let protoc = document.getElementById("proto").value;
 
 import {regisdata} from "./firebaseConnect.js";
-
 open.addEventListener("click", () =>{
     page.style.animation = "ind .6s"
     page.style.opacity = "1";
@@ -20,9 +15,16 @@ open.addEventListener("click", () =>{
 
 })
 
-close.addEventListener("click", () =>{
-    if(nombredev && ip !== null){
-        regisdata(nombredev,ip,protoc)
+close.addEventListener("click", (e) =>{
+    e.preventDefault();
+    let nombredev = document.getElementById("nombre").value;
+    let activo = document.getElementById("activo").checked;
+    let ip = document.getElementById("Ip").value;
+    let protoc = document.getElementById("proto").value;
+    console.log(nombredev + ip + protoc + activo);
+
+    if(nombredev && ip !== ""){
+        regisdata(nombredev,ip,protoc);
         page.style.opacity = "0";
         page.style.pointerEvents = "none";
         page.style.animation = "outd .6s"
