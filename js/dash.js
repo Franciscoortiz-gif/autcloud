3a635d0aa3c300edbb69fc8b7cc90eb2b2d453ff
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-27 19:13:24 -0600

    Add files via upload

---
+++ js/dash.js
@@ -0,0 +1,4 @@
+
+export const nameuser = document.getElementById("username");
+export const emailuser = document.getElementById("useremal");
+export const imageuser = document.getElementById("profid");
