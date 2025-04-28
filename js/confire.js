c47449d65694ef80e6acf4a8aa5403de1adeaf83
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-28 17:59:02 -0600

    Update confire.js

--- js/confire.js
+++ js/confire.js
@@ -1,6 +1,7 @@
-import {} from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-app-compat.js'
-import {} from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-auth-compat.js'
+import {initializeApp} from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js'
+import {getAuth} from 'https://www.gstatic.com/firebasejs/11.6.0/firebase-auth.js'
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAZee6lyFqHynH50WS5BEU8_2MZOSY9bnk",
     authDomain: "cloud-2cab7.firebaseapp.com",
@@ -9,8 +10,6 @@ const firebaseConfig = {
     messagingSenderId: "246856450734",
     appId: "1:246856450734:web:513c712598fc630646a33f",
     measurementId: "G-N1XRBSQYJ5"
-};
-
-firebase.initializeApp(firebaseConfig);
-
-export const auth = firebase.auth();
+  };
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
