7e806044892fd071bcb5b900138a9b00ebc88ea6
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-28 17:51:41 -0600

    Delete js directory

--- js/dash.js
+++
@@ -1,4 +0,0 @@
-
-export const nameuser = document.getElementById("username");
-export const emailuser = document.getElementById("useremal");
-export const imageuser = document.getElementById("profid");
