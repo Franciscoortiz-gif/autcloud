fa0273a6737c50a022c9892f144b3ae742b983be
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-27 18:29:10 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,15 +3,32 @@ export const nameuser = document.getElementById("username");
 export const emailuser = document.getElementById("useremal");
 export const imageuser = document.getElementById("profid");
 
-export const open = document.getElementById("add");
-export const page = document.getElementById("addnew");
-export const close = document.getElementById("save");
+const open = document.getElementById("add");
+const page = document.getElementById("addnew");
+const close = document.getElementById("save");
+const nombredev = document.getElementById("nombre").value;
+let activo = document.getElementById("activo").value;
+let ip = document.getElementById("Ip").value;
+let protoc = document.getElementById("proto").value;
+
+import {regisdata} from "./firebaseConnect.js";
 
 open.addEventListener("click", () =>{
-    page.classList.add('show');
-    alert("prueba");
+    page.style.animation = "ind .6s"
+    page.style.opacity = "1";
+    page.style.pointerEvents = "auto";
+
 })
 
 close.addEventListener("click", () =>{
-    page.classList.remove('show');
+    if(nombredev && ip !== null){
+        regisdata(nombredev,ip,protoc)
+        page.style.opacity = "0";
+        page.style.pointerEvents = "none";
+        page.style.animation = "outd .6s"
+    }else{
+        alert("Los campos no puedes estar vacios");
+        console.log(nombredev + ip + protoc + activo);
+    }
+    
 })
