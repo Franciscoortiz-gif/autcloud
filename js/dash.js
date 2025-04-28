b47ec54b670167977759a8a09990b3382618cf46
Franciscoortiz-gif <franciscoortz575@gmail.com>
2025-04-28 17:55:07 -0600

    Update dash.js

--- js/dash.js
+++ js/dash.js
@@ -1,4 +1,7 @@
-
+import {usercurrent} from "./login.js";
 export const nameuser = document.getElementById("username");
 export const emailuser = document.getElementById("useremal");
 export const imageuser = document.getElementById("profid");
+const useremail = usercurrent.email;
+
+emailuser.innerHTML = useremail;
